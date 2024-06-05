10553eb08f5a181820c494086a38f3d4662f774b
nikita <nikitos7626@mail.ru>
2024-06-05 12:12:57 +0300

    Добавлено поле Отчёты для админа

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../index";
 import "./NavBar.css";
 import { NavLink } from "react-router-dom";
-import { Admin_Route, AddAttraction_route, MainMenu_Route, login_route, Attractions_route, Profile_route } from "../utils/consts";
+import { Admin_Route,MainMenu_Route, login_route, Attractions_route,Attendance_route } from "../utils/consts";
 import { observer } from 'mobx-react-lite';
 import { useNavigate } from 'react-router-dom';
 
@@ -53,6 +53,14 @@ const NavBar = observer(() => {
               <li className="nav-menu-item">
                 <NavLink to={Admin_Route} className="nav-menu-link">
                   Добавить аттракцион
+                </NavLink>
+              </li>
+              
+            )}
+            {user.user.role ==='ADMIN'&& (
+              <li className="nav-menu-item">
+                <NavLink to ={Attendance_route} className="nav-menu-link">
+                  Отчёт
                 </NavLink>
               </li>
             )}
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
-import { Component } from "react"
 import Admin from "./pages/Admin"
 import AddAttraction from "./pages/AddAttractions"
-import { AddAttraction_route, Admin_Route, Attractions_route, MainMenu_Route,Profile_route,login_route} from "./utils/consts"
+import { AddAttraction_route, Admin_Route, Attendance_route, Attractions_route, MainMenu_Route,Profile_route,login_route} from "./utils/consts"
 import Mainmenu from "./pages/MainMenu"
 import Auth from "./pages/Auth"
 import Attractions from "./pages/Attractions"
 import Profile from "./pages/Profile"
 import Report from "./pages/Report"
-import UsersAdmin from "./pages/UsersAdmin"
+
 export const authRoutes= [
     {
         path: Admin_Route,
@@ -18,12 +17,8 @@ export const authRoutes= [
         Component : AddAttraction
     },
     {
-        path: '/admin/report',
+        path:Attendance_route,
         Component: Report
-    },
-    {
-        path: '/admin/users',
-        Component: UsersAdmin
     }
 ]
 
